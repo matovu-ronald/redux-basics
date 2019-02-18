1e38400d853d91106e8c8538367d24175fa76a48
Matovu Ronald <matovuronald997@gmail.com>
2019-02-18 14:05:51 +0300

    Passing and Retriving Data with Action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,24 @@ const reducer = (state = initialState, action) => {
         }
     }
 
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + action.val
+        }
+    }
+
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - action.val
+        }
+    }
+
     return state;
 };
 
